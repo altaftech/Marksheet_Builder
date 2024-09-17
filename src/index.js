b260465e4efa71dacb57b15ed81c4925db15a5e8
Altaf Ali <altafkhanredmi@gmail.com>
2024-09-17 17:45:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Marksheet from './Marksheet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <Marksheet/> */}
+  {/* <App /> */}
+    <Marksheet/>
   </React.StrictMode>
 );
 
